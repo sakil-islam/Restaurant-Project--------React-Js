4e50099e32a734c1c90de6c721b7f11ed5932498
Sakil Islam <sakilislam1080@gmail.com>
2021-05-12 00:28:58 +0600

    Updating Redux Store From CommentForm

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -1,27 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
-const mapStateToProps = state => {
-    //console.log("mapStateToProps: ", state);
-    return {
-        dishes: state.dishes,
-        sample: state.sample
-    }
-}
 
 class Home extends Component {
-    componentDidMount() {
-        console.log("Home Props: ", this.props);
-        this.props.dispatch({
-            type: 'TEST',
-            str: "Sakil"
-        })
-    }
-
-    componentDidUpdate(){
-        console.log("Home Props Updated: ", this.props);
-    }
-
     render() {
         document.title = "Restaurant Project";
         return (
@@ -32,4 +11,4 @@ class Home extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Home);
+export default Home;
