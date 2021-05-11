e8d13a90ec95fe542a7ca6c2d5a2de6edd787537
Sakil Islam <sakilislam1080@gmail.com>
2021-05-11 22:14:01 +0600

    Dispatching Action From Component

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
-    console.log("mapStateToProps: ", state);
+    //console.log("mapStateToProps: ", state);
     return {
         dishes: state.dishes,
         sample: state.sample
@@ -12,7 +12,16 @@ const mapStateToProps = state => {
 class Home extends Component {
     componentDidMount() {
         console.log("Home Props: ", this.props);
+        this.props.dispatch({
+            type: 'TEST',
+            str: "Sakil"
+        })
     }
+
+    componentDidUpdate(){
+        console.log("Home Props Updated: ", this.props);
+    }
+
     render() {
         document.title = "Restaurant Project";
         return (
